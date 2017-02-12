0d904a88cd703e20ad634cc10783bf7ed583e92d
Felipe Z Homma <spacetug@gmail.com>
2017-02-12 15:24:38 -0800

    fixed gst process termination

--- main.go
+++ main.go
@@ -8,7 +8,13 @@ import (
 	"strings"
 )
 
-var running_cmds = map[string]*exec.Cmd{}
+type cmd_data struct {
+	gst_pid      string
+	stop_channel chan string
+}
+
+var running_cmds = map[string]*cmd_data{}
+
 var (
 	CAM_START = make(chan *http.Request)
 	CAM_STOP  = make(chan string)
@@ -17,18 +23,17 @@ var (
 
 func stop_cam(client string) {
 	if running_cmds[client] != nil {
-		log.Println("Killing process\n")
-		running_cmds[client].Process.Kill()
+		log.Printf("Killing process [pid=%s]\n", running_cmds[client].gst_pid)
+		running_cmds[client].stop_channel <- client
 		running_cmds[client] = nil
 	}
 }
 
 func start_cam(client string, port string) {
 	cmd := exec.Command("sh", "/home/ubuntu/cam-test.sh", client, port)
-	err := cmd.Start()
+	out, err := cmd.CombinedOutput()
 
 	if err != nil {
-		cmd = nil
 		FEEDBACK <- err.Error()
 	} else {
 		html := `<!doctype html>
@@ -38,15 +43,22 @@ func start_cam(client string, port string) {
 			<title>Jetson Camera Status</title>
 		</head>
 		<body>
-			<p>Camera Initialized [pid=%d]!</p>
+			<p>Camera Initialized [pid=%s]!</p>
 			<p>Use:</p>
-			<p>gst-launch-1.0 udpsrc port=%s caps="application/x-rtp,media=(string)video,clock-rate=(int)90000, encoding-name=(string)VP8-DRAFT-IETF-01,payload=(int)96" ! rtpvp8depay ! vp8dec ! d3dvideosink </p>
+			<textarea>gst-launch-1.0 udpsrc port=%s caps="application/x-rtp,media=(string)video,clock-rate=(int)90000, encoding-name=(string)VP8-DRAFT-IETF-01,payload=(int)96" ! rtpvp8depay ! vp8dec ! d3dvideosink </textarea>
 		</body>
 	</html>`
 
-		running_cmds[client] = cmd
-		FEEDBACK <- fmt.Sprintf(html, cmd.Process.Pid, port)
-		cmd.Wait()
+		pid := string(out[:len(out)-1])
+		log.Printf("Running at [pid=%s]\n", pid)
+		data := cmd_data{pid, make(chan string)}
+		running_cmds[client] = &data
+		FEEDBACK <- fmt.Sprintf(html, pid, port)
+		<-data.stop_channel
+		killcmd := exec.Command("kill", pid)
+		killcmd.Start()
+		killcmd.Wait()
+		log.Printf("[pid=%s] finished!\n", pid)
 	}
 }
 
@@ -94,7 +106,7 @@ func handler_help(w http.ResponseWriter, r *http.Request) {
 	</html>`
 	client_list := ""
 	for k, v := range running_cmds {
-		line := fmt.Sprintf("<p>client='%s' pid=%d</p><br>", k, v.Process.Pid)
+		line := fmt.Sprintf("<p>client='%s' pid=%d</p><br>", k, v.gst_pid)
 		client_list = fmt.Sprintf("%s%s", client_list, line)
 	}
 	fmt.Fprintf(w, html, client, client_list)
